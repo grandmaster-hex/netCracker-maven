954724f20fdca60d79e3dd6e3819bf999e80f0aa
Turin <@192.168.1.3>
2012-06-13 19:41:21 +0300

    * web.xml [faces/index.xhtml]

--- WayToNetCracker-web/src/main/java/beans/Login.java
+++ WayToNetCracker-web/src/main/java/beans/Login.java
@@ -15,26 +15,68 @@ import javax.enterprise.context.SessionScoped;
 @SessionScoped
 public class Login implements Serializable {
 
-    String login;
-    String password;
+    String loginAdmin;
+    String loginHr;
+    String loginInter;
+    String passwordAdmin;
+    String passwordHr;
+    String passwordInter;
 
-    public String getLogin() {
-        return login;
+    public String getLoginAdmin() {
+        return loginAdmin;
     }
 
-    public void setLogin(String login) {
-        this.login = login;
+    public void setLoginAdmin(String loginAdmin) {
+        this.loginAdmin = loginAdmin;
     }
 
-    public String getPassword() {
-        return password;
+    public String getLoginHr() {
+        return loginHr;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setLoginHr(String loginHr) {
+        this.loginHr = loginHr;
+    }
+
+    public String getLoginInter() {
+        return loginInter;
+    }
+
+    public void setLoginInter(String loginInter) {
+        this.loginInter = loginInter;
+    }
+
+    public String getPasswordAdmin() {
+        return passwordAdmin;
+    }
+
+    public void setPasswordAdmin(String passwordAdmin) {
+        this.passwordAdmin = passwordAdmin;
+    }
+
+    public String getPasswordHr() {
+        return passwordHr;
+    }
+
+    public void setPasswordHr(String passwordHr) {
+        this.passwordHr = passwordHr;
+    }
+
+    public String getPasswordInter() {
+        return passwordInter;
+    }
+
+    public void setPasswordInter(String passwordInter) {
+        this.passwordInter = passwordInter;
     }
 
     public String checkAdmin() {
+        return "index.xhtml";
+    }
+    public String checkHr() {
+        return "index.xhtml";
+    }
+    public String checkInter() {
         return "index.xhtml";
     }
 }
