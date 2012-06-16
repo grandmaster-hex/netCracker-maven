a31caa8b9854640c9233ed814c340e5dd7d9cc89
Slavik Leonov <onlylastride@bigmir.net>
2012-06-16 21:11:21 +0300

    Blob finished

--- WayToNetCracker-ejb/src/main/java/netcracker/testing/employeedaotesting.java
+++ WayToNetCracker-ejb/src/main/java/netcracker/testing/employeedaotesting.java
@@ -20,7 +20,7 @@ import org.apache.poi.util.IOUtils;
  * @author lasha.k
  */
 public class employeedaotesting {
-    public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args){
         try {
             //---------------Testing getingAllUniversities();
                     
@@ -46,30 +46,43 @@ public class employeedaotesting {
             //        System.out.print(f.toString());
             //     
             //-------------------Testing Form
-                  Calendar cal = Calendar.getInstance();
-                  cal.set(Calendar.YEAR,2007);
-                  Date b = cal.getTime();
-                  System.out.print(b.toString());
-            //      Student a = new Student("Анна", "Кудра", "Алексеевна",5,b,1,"violatormode@rambler.ru","phdhospital@mail.ru","050-886-16-27",
-                     //     null, "beacause I'm the best", "no expirience",null,0);
-                    
-            //---------------Testing createEmployees();
-            //   EmployeeDAO d = null;
-            //   d = DAOFactory.getEmployeeDAO();  
-            //   boolean result = d.createEmployee("phd", "13adsf", "Иван", "Дебилов", "phdhospital@gmail.com", 1);
-            //   if(result)
-            //       System.out.print("\nCreate Successful");
-            //   System.out.print("\n\n\n"+d.getEmployeeById(1).getFirstName());
-            //---------------Testing deleteEmployeeById()
-            //   d.deleteEmployeeById(2);
-            //---------------Testing sending mail with attachment   
-            //       MailBean mail = new MailBean();
-            //        try{
-            //        mail.sendForm("kvata.l@gmail.com, kalininasofiya@gmail.com,kac.anna.vl@gmail.com,onlylastride@gmail.com,lebentrop@gmail.com","/home/phd/ssh.txt");
-            //        }
-            //        catch (Exception ex){
-            //            System.out.print("Something wrong");
-            //        }
+      Calendar cal = Calendar.getInstance();
+      cal.set(Calendar.YEAR,2007);
+      java.sql.Date sqlDate = new java.sql.Date(new java.util.Date().getTime());
+                 // System.out.print(b.toString());
+                  
+      File file = new File("/home/lastride/vihyhol.jpg");  
+      FileInputStream fis = new FileInputStream(file);  
+      byte b1[] = new byte[(int)file.length()];  
+      fis.read(b1);  
+      System.out.println(b1.length);  
+            try {
+                java.sql.Blob b2 = new SerialBlob(b1);  
+                Student s1 = new Student(2, "asd", "asd", "asd", 4, sqlDate, 1, "asd", "fgh", "gfh", "hjg", "kj", "hgj", "kj", b2);        
+                StudentDAO stud = DAOFactory.getStudentDAO();
+                 stud.createStudent(s1);
+                      //---------------Testing createEmployees();
+                      //   EmployeeDAO d = null;
+                      //   d = DAOFactory.getEmployeeDAO();  
+                      //   boolean result = d.createEmployee("phd", "13adsf", "Иван", "Дебилов", "phdhospital@gmail.com", 1);
+                      //   if(result)
+                      //       System.out.print("\nCreate Successful");
+                      //   System.out.print("\n\n\n"+d.getEmployeeById(1).getFirstName());
+                      //---------------Testing deleteEmployeeById()
+                      //   d.deleteEmployeeById(2);
+                      //---------------Testing sending mail with attachment   
+                      //       MailBean mail = new MailBean();
+                      //        try{
+                      //        mail.sendForm("kvata.l@gmail.com, kalininasofiya@gmail.com,kac.anna.vl@gmail.com,onlylastride@gmail.com,lebentrop@gmail.com","/home/phd/ssh.txt");
+                      //        }
+                      //        catch (Exception ex){
+                      //        }
+                      //        }
+            } catch (SerialException ex) {
+                Logger.getLogger(employeedaotesting.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SQLException ex) {
+                Logger.getLogger(employeedaotesting.class.getName()).log(Level.SEVERE, null, ex);
+            }
         } catch (IOException ex) {
             Logger.getLogger(employeedaotesting.class.getName()).log(Level.SEVERE, null, ex);
         }
