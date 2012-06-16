14299e2aab8aa3fcac2ef7269e74a98fabba9282
Lasha Kvatashydze <kvata.l@gmail.com>
2012-06-16 19:49:11 +0300

    changes

--- WayToNetCracker-ejb/src/main/java/netcracker/dao/DAOFactory.java
+++ WayToNetCracker-ejb/src/main/java/netcracker/dao/DAOFactory.java
@@ -1,21 +1,14 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package netcracker.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import netcracker.dao.*;
-
-
 
 /**
- *
- * @author lasha.k
- */
+*
+* @author lasha.k
+*/
 public final class DAOFactory {
     private DAOFactory(){
         
@@ -29,60 +22,52 @@ public final class DAOFactory {
         return new FormDAOImpl();
     }
     
-    public static SkillsDAOImpl getSkillsDAO() {
-        return new SkillsDAOImpl();
-    }
-    
     public static InterviewDAO getInterviewDAO() {
         return new InterviewDAOImpl();
-    }
-    
-    public static InterestDAO getInterestDAO(){
-        return new InterestDAOImpl();
     }
     //Create java.sql.Connection with params set in DAOConstants.java
     public static Connection createConnection(){
             Connection conn = null;
-		 try
-		 {
+try
+{
                      Class.forName(DAOConstants.driver).newInstance();
                      conn = DriverManager.getConnection(DAOConstants.url, DAOConstants.user, DAOConstants.password);
-                    // System.out.print("\nDB connection established! \n");
-		 }
-		 catch (Exception ex)
-		 {
+                     System.out.print("\nDB connection established! \n");
+}
+catch (Exception ex)
+{
                         System.err.print("\nCannot connect to DB!\n");
-		 }
+}
                  return conn;
     }
     //Closes opened java.sql.Connection or throws SQLException
     public static void closeConnection(final Connection conn)
-	{
-		if (conn != null)
-		{
-			 try
-			 {
-				conn.close();
-			 }
-			 catch (SQLException ex)
-			 {
-				System.out.print("Close connection error");
-			 }
-		}
-	}
+{
+if (conn != null)
+{
+try
+{
+conn.close();
+}
+catch (SQLException ex)
+{
+System.out.print("Close connection error");
+}
+}
+}
     // closes opened stmt or throws SQLException
     public static void closeStatement(final PreparedStatement stmt)
-	{
-		if (stmt != null)
-		{
-			 try
-			 {
-				stmt.close();
-			 }
-			 catch (SQLException ex)
-			 {
-				System.out.print("Close statement error");
-			 }
-		}
-	}
+{
+if (stmt != null)
+{
+try
+{
+stmt.close();
+}
+catch (SQLException ex)
+{
+System.out.print("Close statement error");
+}
+}
+}
 }
