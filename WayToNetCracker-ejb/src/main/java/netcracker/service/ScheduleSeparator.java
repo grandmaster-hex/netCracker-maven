6af3760f14fc0d4c465a2a3ba5c800f532bfb6de
Lasha Kvatashydze <kvata.l@gmail.com>
2012-06-17 15:15:26 +0300

    creates list<Shedule> and returns to createShedule();

--- WayToNetCracker-ejb/src/main/java/netcracker/service/ScheduleSeparator.java
+++ WayToNetCracker-ejb/src/main/java/netcracker/service/ScheduleSeparator.java
@@ -5,27 +5,29 @@
 package netcracker.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-
+import netcracker.dao.DAOFactory;
+import netcracker.dao.Shedule;
 /**
  *
  * @author AnnKats
  */
 public class ScheduleSeparator {
     
-    public static List<Date> separate(Date begin, Date end, int duration) {
-        
-        List<Date> list = new ArrayList<Date>();
-        
+    public static void separate(Date begin, Date end, int duration,int interval_status,int interviewers_count) {
+               
+        List<Shedule> she = new ArrayList<Shedule>();
         long msBegin = begin.getTime();
         long msEnd = end.getTime();
         long msDuration = duration * 60 * 1000;
         
         for(long tm = msBegin; tm + msDuration <= msEnd; tm += msDuration) {
-            list.add(new Date(tm));
+            she.add(new Shedule(new java.sql.Date(new Date(tm).getTime()), 
+                    new java.sql.Date(new Date(tm+msDuration).getTime()),interviewers_count,interval_status));
         }
-        
-        return list;
+        DAOFactory.getSheduleDAO().createShedule(she);
     }
+   
 }
