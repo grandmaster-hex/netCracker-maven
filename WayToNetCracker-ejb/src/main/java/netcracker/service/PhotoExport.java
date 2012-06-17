3d8cca9a8a6a7254b429c08c254e26b6704aa592
Slavik Leonov <onlylastride@bigmir.net>
2012-06-17 13:44:55 +0300

    PhotoExport finished, ask what to return

--- WayToNetCracker-ejb/src/main/java/netcracker/service/PhotoExport.java
+++ WayToNetCracker-ejb/src/main/java/netcracker/service/PhotoExport.java
@@ -26,21 +26,41 @@ public class PhotoExport {
 
     public static boolean exportPhoto() {
         ZipOutputStream zip = null;
+        ZipEntry entry = null;
+        PreparedStatement stmt = null;
+        PreparedStatement stmtCount = null;
+        ResultSet result = null;
+        ResultSet resultCount = null;
+        Connection conn = DAOFactory.createConnection();
+        StringBuffer sbPhoto = null;
         try {
-            PreparedStatement stmt = null;
-            ResultSet result = null;
-            Connection conn = DAOFactory.createConnection();
-            StringBuffer sbPhoto = null;
             sbPhoto = new StringBuffer();
-            sbPhoto.append("SELECT photo, last_name, first_name FROM ");
+            sbPhoto.append("SELECT photo, last_name, first_name, email1 FROM ");
             sbPhoto.append(DAOConstants.StudentsTableName);
             zip = new ZipOutputStream(new FileOutputStream("/home/lastride/photos.zip"));
             try {
                 stmt = conn.prepareStatement(sbPhoto.toString());
                 result = stmt.executeQuery();
                 while (result.next()) {
-                    ZipEntry entry = new ZipEntry(result.getString("last_name")
-                            + "_" + result.getString("first_name") + ".jpeg");
+                    StringBuffer sbStudentsCount = new StringBuffer();
+                    sbStudentsCount.append("SELECT count(id_student) FROM ");
+                    sbStudentsCount.append(DAOConstants.StudentsTableName);
+                    sbStudentsCount.append(" WHERE first_name like '%" + result.getString("first_name") + "%'");
+                    sbStudentsCount.append(" AND last_name like '%" + result.getString("last_name") + "%'");
+                    stmtCount = conn.prepareStatement(sbStudentsCount.toString());
+                    resultCount = stmtCount.executeQuery();
+                    while (resultCount.next()) {
+                        if (resultCount.getInt(1) > 1) {
+                            entry = new ZipEntry(result.getString("last_name")
+                                    + "_" + result.getString("first_name") + "_"
+                                    + result.getString("email1") + ".jpeg");
+                        }
+                        else {
+                           entry = new ZipEntry(result.getString("last_name")
+                            + "_" + result.getString("first_name") + ".jpeg"); 
+                        }
+                    }
+                    
                     zip.putNextEntry(entry);
                     zip.write((byte[]) result.getObject("photo"));
                 }
